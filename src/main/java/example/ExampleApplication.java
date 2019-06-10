3c0d66b7ac9dfc3d40646b5b40e44ad4043eee49
Kenny Hoang <kennyhoang2208@gmail.com>
2019-06-10 15:36:26 +0700

    Add Spring cloud denpendencis

--- src/main/java/example/ExampleApplication.java
+++ src/main/java/example/ExampleApplication.java
@@ -3,6 +3,9 @@ package example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableJpaRepositories
 @Configuration
+@EnableDiscoveryClient
+@EnableCircuitBreaker
 @ComponentScan(basePackages = "example")
 public class ExampleApplication {
 
@@ -20,6 +25,7 @@ public class ExampleApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplateBuilder().build();
 	}
