eb0631c97ff468b47efb77403099f912825a36cb
Tuyen Tran <tuyen.tran@asnet.com.vn>
2019-06-24 23:28:23 +0700

    Fix missing the fetching tafs

--- build-latest-version.sh
+++ build-latest-version.sh
@@ -2,6 +2,11 @@
 set -e
 
 echo 'latest' > VERSION
+
+# Fetch new tags
+git fetch --tags
+
+# Check if the latest existed
 if [ ! $(git tag -l vlatest) ]; then
     echo 'Not found. Creating new latest tag.'
     jx step tag --version $(cat VERSION)
@@ -10,6 +15,7 @@ else
     git tag -f "v$(cat VERSION)"
 fi
 
+# Build and push the latest image
 gradle clean build
 export VERSION=`cat VERSION` && skaffold build -f skaffold.yaml
 jx step post build --image $DOCKER_REGISTRY/$ORG/$APP_NAME:\$(cat VERSION)
